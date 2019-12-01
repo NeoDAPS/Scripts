2bc9b7aa6f133364661ea7e11eb8953d86077678
Ghost In The Mask <58383591+GhostInTheMask@users.noreply.github.com>
2019-12-01 09:33:42 +0200

    Add files via upload

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+clear
+echo "Starting DAPS Bootstrap install script"
+echo "Stopping dapscoind..."
+dapscoin-cli stop
+sleep 5
+echo "Downloading bootstrap..."
+wget -N https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
+echo "Removing old blocks, chainstate, and database folders...."
+rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/.lock ~/.dapscoin/dapscoind.pid
+echo "Installing unzip..."
+sudo apt-get install unzip -y
+echo "Installing new blocks folders..."
+unzip bootstrap.zip -d ~/.dapscoin
+echo "Bootstrap installed!"
